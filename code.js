9b4acfdbad13456138c15c57b376f99a38be0e78
Guilherme Rodrigues <gui.rodrigues268@pm.me>
2024-03-23 15:36:29 -0300

    Adicionar estilo e dinamicidade

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
-function cor(){
-    document.getElementById("milho").style.color = "#ff0000";
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const botoes = document.querySelectorAll('.botao');
+    const culturas = document.querySelectorAll('.cultura');
+
+    botoes.forEach((botao, indice) => {
+        botao.addEventListener("click", () =>{
+            
+            desmarcarBtn();
+            desmarcarCult();
+            
+            botao.classList.add("selecionado");
+            culturas[indice].classList.add("ativa");
+        });
+    });
+});
+
+function desmarcarBtn(){
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
 }
+
+function desmarcarCult(){
+     const cultSelecionada = document.querySelector(".cultura.ativa");
+     cultSelecionada.classList.remove("ativa");
+ }
